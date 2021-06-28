79a55f547630395c26d207d5f8ab2401280cff41
JhonasML <jhonas.mutton@mercadolivre.com>
2021-06-28 18:22:15 -0300

    exercicio aula 1

---
+++ exercicios/src/aula1/exercicio.java
@@ -0,0 +1,20 @@
+package aula1;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class exercicio {
+    public static void main(String[] args) {
+        Integer[] vetor = {52, 10, 85, 1324, 1, 13, 62, 30, 12, 127};
+        Arrays.sort(vetor);
+
+        System.out.println("Ordem crescente:");
+        Arrays.stream(vetor).forEach(System.out::println);
+
+        Arrays.sort(vetor, Comparator.reverseOrder());
+
+        System.out.println("Ordem decrescente:");
+        Arrays.stream(vetor).forEach(System.out::println);
+
+    }
+}
